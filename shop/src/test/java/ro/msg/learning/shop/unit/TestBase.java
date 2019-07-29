ee332e84e6730fbceff9144fb1e88c8be5d2525b
ratiuk <kinga.ratiu@yahoo.com>
2019-07-29 12:59:54 +0300

    Fix Codacy Warnings

--- shop/src/test/java/ro/msg/learning/shop/unit/TestBase.java
+++ shop/src/test/java/ro/msg/learning/shop/unit/TestBase.java
@@ -9,33 +9,33 @@ import java.util.stream.Stream;
 
 public class TestBase {
 
-    ProductCategory productCategory = ProductCategory.builder().name("furniture").description("products in it").build();
+    protected ProductCategory productCategory = ProductCategory.builder().name("furniture").description("products in it").build();
 
-    Supplier supplier = Supplier.builder().name("Ikea").build();
+    protected Supplier supplier = Supplier.builder().name("Ikea").build();
 
-    Product product = Product.builder().name("Dining table").description("Beautiful table :)").price(BigDecimal.valueOf(129.99)).weight(12.5).imageURL("forget the URL").category(productCategory).supplier(supplier).build();
+    protected Product product = Product.builder().name("Dining table").description("Beautiful table :)").price(BigDecimal.valueOf(129.99)).weight(12.5).imageURL("forget the URL").category(productCategory).supplier(supplier).build();
 
-    Product product2 = Product.builder().name("Dining table 2").description("Beautiful table :)").price(BigDecimal.valueOf(149.99)).weight(18.5).imageURL("forget the URL").category(productCategory).supplier(supplier).build();
+    protected Product product2 = Product.builder().name("Dining table 2").description("Beautiful table :)").price(BigDecimal.valueOf(149.99)).weight(18.5).imageURL("forget the URL").category(productCategory).supplier(supplier).build();
 
-    Address address = Address.builder().addressCountry("Germany").addressCounty("Bayern").addressCity("Munchen").addressStreet("Neuburger 22").build();
+    protected Address address = Address.builder().addressCountry("Germany").addressCounty("Bayern").addressCity("Munchen").addressStreet("Neuburger 22").build();
 
-    Location location = Location.builder().name("MUC").address(address).build();
+    protected Location location = Location.builder().name("MUC").address(address).build();
 
-    Location location2 = Location.builder().name("MUC2").address(address).build();
+    protected Location location2 = Location.builder().name("MUC2").address(address).build();
 
-    Stock stock = Stock.builder().location(location).product(product).quantity(10).build();
+    protected Stock stock = Stock.builder().location(location).product(product).quantity(10).build();
 
-    Stock stock2 = Stock.builder().location(location2).product(product).quantity(100).build();
+    protected Stock stock2 = Stock.builder().location(location2).product(product).quantity(100).build();
 
-    Stock stock3 = Stock.builder().location(location).product(product2).quantity(10).build();
+    protected Stock stock3 = Stock.builder().location(location).product(product2).quantity(10).build();
 
-    List<Stock> stocks = Stream.of(stock, stock2, stock3).collect(Collectors.toList());
+    protected List<Stock> stocks = Stream.of(stock, stock2, stock3).collect(Collectors.toList());
 
-    List<Stock> stocksProd = Stream.of(stock2, stock).collect(Collectors.toList());
+    protected List<Stock> stocksProd = Stream.of(stock2, stock).collect(Collectors.toList());
 
-    List<Stock> stocksProdBigStock = Stream.of(stock2).collect(Collectors.toList());
+    protected List<Stock> stocksProdBigStock = Stream.of(stock2).collect(Collectors.toList());
 
-    List<Stock> stocksProd2 = Stream.of(stock3).collect(Collectors.toList());
+    protected List<Stock> stocksProd2 = Stream.of(stock3).collect(Collectors.toList());
 
 
      void setIds() {
